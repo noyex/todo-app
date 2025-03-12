3b60b0482e014fa176e4a66fcbba868da4685f24
noyex <153921909+noyex@users.noreply.github.com>
2025-03-12 10:42:27 +0100

    done

--- todo-data/src/main/java/com/noyex/tododata/DTOs/RegisterUserDTO.java
+++ todo-data/src/main/java/com/noyex/tododata/DTOs/RegisterUserDTO.java
@@ -1,43 +1,15 @@
 package com.noyex.tododata.DTOs;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Data
 public class RegisterUserDTO {
 
     private String username;
     private String email;
     private String password;
 
-    public RegisterUserDTO(String username, String email, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
